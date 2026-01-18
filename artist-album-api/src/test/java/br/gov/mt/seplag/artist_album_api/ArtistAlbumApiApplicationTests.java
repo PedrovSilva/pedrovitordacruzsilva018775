81c31f2555339f92c36342a56ccc470cef3cda3a
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2026-01-18 14:02:33 -0400

    chore: setup test profile

--- artist-album-api/src/test/java/br/gov/mt/seplag/artist_album_api/ArtistAlbumApiApplicationTests.java
+++ artist-album-api/src/test/java/br/gov/mt/seplag/artist_album_api/ArtistAlbumApiApplicationTests.java
@@ -2,12 +2,15 @@ package br.gov.mt.seplag.artist_album_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
+@ActiveProfiles("test")
 class ArtistAlbumApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
