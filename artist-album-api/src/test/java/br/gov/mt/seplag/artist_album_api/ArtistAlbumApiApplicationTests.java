5bd7dad087c316821eb1b555e9b9068c72698a03
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2026-01-18 13:39:00 -0400

    chore: springInitizr template

---
+++ artist-album-api/src/test/java/br/gov/mt/seplag/artist_album_api/ArtistAlbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.artist_album_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistAlbumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
